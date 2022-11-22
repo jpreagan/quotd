a64d20beda47f9bd57e8aec3d1e46e3b43b714fc
James Reagan <james@jpreagan.com>
2022-11-21 14:40:14 -1000

    Define return types for findOne user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { User as UserModel } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async findOne(username: string) {
+  async findOne(username: string): Promise<UserModel> | undefined {
     return this.prisma.user.findUnique({
       where: {
         username,
