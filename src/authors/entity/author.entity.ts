5e5f479994c022e0dfecd922b9415494ace579e5
James Reagan <james@jpreagan.com>
2023-01-10 20:09:11 -1000

    Add author with quotes entity

--- src/authors/entity/author.entity.ts
+++ src/authors/entity/author.entity.ts
@@ -1,8 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { QuoteEntity } from '../../quotes/entity/quote.entity';
 
 export class AuthorEntity {
   @ApiProperty({ example: 1 })
   id: number;
   @ApiProperty({ example: 'Audrey Hepburn' })
   name: string;
+}
+
+export class AuthorWithQuotesEntity {
+  @ApiProperty({ example: 1 })
+  id: number;
+  @ApiProperty({ example: 'Audrey Hepburn' })
+  name: string;
+  @ApiProperty({ isArray: true })
+  quotes: QuoteEntity[];
 }
